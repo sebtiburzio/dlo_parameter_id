10dafdf589978d58c3bee4ec87746d10c46aa3e7
sebtiburzio <sebtiburzio@gmail.com>
2023-07-24 19:37:29 +0200

    FR3 control saves plans and executes separately

--- scripts/fr3_control.py
+++ scripts/fr3_control.py
@@ -7,12 +7,9 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-import rokubimini_msgs.srv
 import tf
 import numpy as np
 from math import pi
-from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
 
 
 def move_home(joint1=0.0,joint6=0.9):
@@ -20,7 +17,7 @@ def move_home(joint1=0.0,joint6=0.9):
   move_group.stop()
 
 
-def move_to_quaternion(x,y,z, qx, qy, qz ,qw):
+def plan_to_quaternion(x,y,z, qx, qy, qz ,qw):
   pose_goal = geometry_msgs.msg.Pose()
   pose_goal.orientation.x = qx
   pose_goal.orientation.y = qy
@@ -30,12 +27,9 @@ def move_to_quaternion(x,y,z, qx, qy, qz ,qw):
   pose_goal.position.y = y
   pose_goal.position.z = z
   move_group.set_pose_target(pose_goal)
-  move_group.go(wait=True)
-  move_group.stop()
-  move_group.clear_pose_targets()
+  plan = move_group.plan()
+  return plan
 
-
-#TODO - figure out why cant plan the exectute separaetly
 def plan_to_extr_XZY(x,y,z, X, Z, Y):
   pose_goal = geometry_msgs.msg.Pose()
   quat = tf.transformations.quaternion_from_euler(X, Z, Y, 'sxzy')
@@ -47,22 +41,23 @@ def plan_to_extr_XZY(x,y,z, X, Z, Y):
   pose_goal.position.y = y
   pose_goal.position.z = z
   move_group.set_pose_target(pose_goal)
-  move_group.plan()
+  plan = move_group.plan()
+  return plan
 
-def move_to_extr_XZY(x,y,z, X, Z, Y):
-  pose_goal = geometry_msgs.msg.Pose()
-  quat = tf.transformations.quaternion_from_euler(X, Z, Y, 'sxzy')
-  pose_goal.orientation.x = quat[0]
-  pose_goal.orientation.y = quat[1]
-  pose_goal.orientation.z = quat[2]
-  pose_goal.orientation.w = quat[3]
-  pose_goal.position.x = x
-  pose_goal.position.y = y
-  pose_goal.position.z = z
-  move_group.set_pose_target(pose_goal)
-  move_group.go(wait=True)
-  move_group.stop()
-  move_group.clear_pose_targets()
+# def move_to_extr_XZY(x,y,z, X, Z, Y):
+#   pose_goal = geometry_msgs.msg.Pose()
+#   quat = tf.transformations.quaternion_from_euler(X, Z, Y, 'sxzy')
+#   pose_goal.orientation.x = quat[0]
+#   pose_goal.orientation.y = quat[1]
+#   pose_goal.orientation.z = quat[2]
+#   pose_goal.orientation.w = quat[3]
+#   pose_goal.position.x = x
+#   pose_goal.position.y = y
+#   pose_goal.position.z = z
+#   move_group.set_pose_target(pose_goal)
+#   move_group.go(wait=True)
+#   move_group.stop()
+#   move_group.clear_pose_targets()
 
 def plan_to(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
   pose_goal = geometry_msgs.msg.Pose()
@@ -75,22 +70,29 @@ def plan_to(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
   pose_goal.position.y = y
   pose_goal.position.z = z
   move_group.set_pose_target(pose_goal)
-  move_group.plan()
+  plan = move_group.plan()
+  return plan
 
-def move_to(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
-  pose_goal = geometry_msgs.msg.Pose()
-  quat = tf.transformations.quaternion_from_euler(a1, a2, a3, r + ax1 + ax2 + ax3)
-  pose_goal.orientation.x = quat[0]
-  pose_goal.orientation.y = quat[1]
-  pose_goal.orientation.z = quat[2]
-  pose_goal.orientation.w = quat[3]
-  pose_goal.position.x = x
-  pose_goal.position.y = y
-  pose_goal.position.z = z
-  move_group.set_pose_target(pose_goal)
-  move_group.go(wait=True)
-  move_group.stop()
-  move_group.clear_pose_targets()
+# def move_to(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
+#   pose_goal = geometry_msgs.msg.Pose()
+#   quat = tf.transformations.quaternion_from_euler(a1, a2, a3, r + ax1 + ax2 + ax3)
+#   pose_goal.orientation.x = quat[0]
+#   pose_goal.orientation.y = quat[1]
+#   pose_goal.orientation.z = quat[2]
+#   pose_goal.orientation.w = quat[3]
+#   pose_goal.position.x = x
+#   pose_goal.position.y = y
+#   pose_goal.position.z = z
+#   move_group.set_pose_target(pose_goal)
+#   move_group.go(wait=True)
+#   move_group.stop()
+#   move_group.clear_pose_targets()
+
+def execute_plan(plan):
+    move_group.execute(plan, wait=True)
+    move_group.stop()
+    move_group.clear_pose_targets()
+    plan = None
 
 def plan_to_cart(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
   waypoints = []
@@ -105,39 +107,44 @@ def plan_to_cart(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
   pose_goal.position.z = z
   waypoints.append(copy.deepcopy(pose_goal))
   (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0) # jump_threshold - TODO check if should change
-
-def move_to_cart(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
-  waypoints = []
-  pose_goal = geometry_msgs.msg.Pose()
-  quat = tf.transformations.quaternion_from_euler(a1, a2, a3, r + ax1 + ax2 + ax3)
-  pose_goal.orientation.x = quat[0]
-  pose_goal.orientation.y = quat[1]
-  pose_goal.orientation.z = quat[2]
-  pose_goal.orientation.w = quat[3]
-  pose_goal.position.x = x
-  pose_goal.position.y = y
-  pose_goal.position.z = z
-  waypoints.append(copy.deepcopy(pose_goal))
-  (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0) # jump_threshold - TODO check if should change
-  plan = move_group.retime_trajectory(move_group.get_current_state(),plan,0.2,0.2)
+  plan = move_group.retime_trajectory(move_group.get_current_state(),plan,0.03,0.03)
   if fraction == 1.0:
-    move_group.execute(plan, wait=True)
+    return plan
   else:
     print("ERROR: planning failed at %d of trajectory", fraction)
+
+# def move_to_cart(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
+#   waypoints = []
+#   pose_goal = geometry_msgs.msg.Pose()
+#   quat = tf.transformations.quaternion_from_euler(a1, a2, a3, r + ax1 + ax2 + ax3)
+#   pose_goal.orientation.x = quat[0]
+#   pose_goal.orientation.y = quat[1]
+#   pose_goal.orientation.z = quat[2]
+#   pose_goal.orientation.w = quat[3]
+#   pose_goal.position.x = x
+#   pose_goal.position.y = y
+#   pose_goal.position.z = z
+#   waypoints.append(copy.deepcopy(pose_goal))
+#   (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0) # jump_threshold - TODO check if should change
+#   plan = move_group.retime_trajectory(move_group.get_current_state(),plan,0.2,0.2)
+#   if fraction == 1.0:
+#     move_group.execute(plan, wait=True)
+#   else:
+#     print("ERROR: planning failed at %d of trajectory", fraction)
  
 
-def plan_cart_path(xs,zs,phis,execute=False):
+def plan_cart_path(xs,zs,phis):
   waypoints = []
 
   for n in range(len(xs)):
     pose_goal = geometry_msgs.msg.Pose()
-    quat = tf.transformations.quaternion_from_euler(pi, pi/4, phis[n], 'sxzy')
+    quat = tf.transformations.quaternion_from_euler(pi, 0, phis[n], 'sxzy')
     pose_goal.orientation.x = quat[0]
     pose_goal.orientation.y = quat[1]
     pose_goal.orientation.z = quat[2]
     pose_goal.orientation.w = quat[3]
     pose_goal.position.x = xs[n]
-    pose_goal.position.y = 0.0
+    pose_goal.position.y = -0.2
     pose_goal.position.z = zs[n]
     print(pose_goal)
     print(waypoints)
@@ -146,26 +153,24 @@ def plan_cart_path(xs,zs,phis,execute=False):
   (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0) # jump_threshold - TODO check if should change
   plan = move_group.retime_trajectory(move_group.get_current_state(),plan,0.03,0.03)
   if fraction == 1.0:
-    print("planning success")
-    if execute:
-      move_group.execute(plan, wait=True)
+    return plan
   else:
     print("ERROR: planning failed at %d of trajectory", fraction)
 
 
-def align_ft_z_at(x=0.3,y=0.0,z=0.55):
-  pose_goal = geometry_msgs.msg.Pose()
-  pose_goal.orientation.x = 1.0
-  pose_goal.orientation.y = 0.0
-  pose_goal.orientation.z = 0.0
-  pose_goal.orientation.w = 0.0
-  pose_goal.position.x = x
-  pose_goal.position.y = y
-  pose_goal.position.z = z
-  move_group.set_pose_target(pose_goal)
-  move_group.go(wait=True)
-  move_group.stop()
-  move_group.clear_pose_targets()
+# def align_ft_z_at(x=0.3,y=0.0,z=0.55):
+#   pose_goal = geometry_msgs.msg.Pose()
+#   pose_goal.orientation.x = 1.0
+#   pose_goal.orientation.y = 0.0
+#   pose_goal.orientation.z = 0.0
+#   pose_goal.orientation.w = 0.0
+#   pose_goal.position.x = x
+#   pose_goal.position.y = y
+#   pose_goal.position.z = z
+#   move_group.set_pose_target(pose_goal)
+#   move_group.go(wait=True)
+#   move_group.stop()
+#   move_group.clear_pose_targets()
 
 #%%
 
@@ -195,8 +200,11 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
                                               queue_size=20)
 
 #%%
-path = np.loadtxt('./static_solver_path.csv', dtype=np.float64, delimiter=',')
+path = np.loadtxt('./orange_grid_horiz_RHS.csv', dtype=np.float64, delimiter=',')
 xs = path[:,0]
 zs = path[:,1]
 phis = path[:,2]
-plan_cart_path(xs,zs,phis,execute=False)
+plan = plan_to_cart(xs[0], -0.2, zs[0], pi, 0, phis[0])
+#%%
+plan = plan_cart_path(xs,zs,phis)
+# %%
