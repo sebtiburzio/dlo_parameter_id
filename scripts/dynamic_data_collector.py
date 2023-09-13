8813c9db49576d794570fe7b2e0e67a635c15302
sebtiburzio <sebtiburzio@gmail.com>
2023-09-13 19:00:24 +0200

    Data for dynamic comparing to model fwd sim Updates various scripts for consistency

--- scripts/dynamic_data_collector.py
+++ scripts/dynamic_data_collector.py
@@ -1,18 +1,53 @@
 #!/usr/bin/env python3
 
+#%%
 import sys
 import copy
 import rospy
-import dynamic_reconfigure.client
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 import rokubimini_msgs.srv
+import tf
 from math import pi
-from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
 
+## Functions to move the robot around
+def move_home(joint1=0.0,joint6=1.1):
+  if joint1 > 0.0:
+    joint1 = 0.0
+    print("Clamping joint1 below 0.0 rad")
+  elif joint1 < -2.5:
+    joint1 = -2.5
+    print("Clamping joint1 above -2.5 rad")
+  move_group.go(joints=[joint1, -0.55, 0.0, -1.65, 0.0, joint6, -pi/2], wait=True)
+  move_group.stop()
 
+def plan_to_cart(x,y,z, a1, a2, a3, ax1='x', ax2='z', ax3='y', r='s'):
+  waypoints = []
+  pose_goal = geometry_msgs.msg.Pose()
+  quat = tf.transformations.quaternion_from_euler(a1, a2, a3, r + ax1 + ax2 + ax3)
+  pose_goal.orientation.x = quat[0]
+  pose_goal.orientation.y = quat[1]
+  pose_goal.orientation.z = quat[2]
+  pose_goal.orientation.w = quat[3]
+  pose_goal.position.x = x
+  pose_goal.position.y = y
+  pose_goal.position.z = z
+  waypoints.append(copy.deepcopy(pose_goal))
+  (plan, fraction) = move_group.compute_cartesian_path(waypoints, 0.01, 0.0)
+  plan = move_group.retime_trajectory(move_group.get_current_state(),plan,0.1,0.1)
+  if fraction == 1.0:
+    return plan
+  else:
+    print("ERROR: planning failed at %d of trajectory", fraction)
+
+def execute_plan(plan):
+  move_group.execute(plan, wait=True)
+  move_group.stop()
+  move_group.clear_pose_targets()
+  plan = None
+
+## Functions to calibrate the FT sensor
 def align_ft_z_at(x=0.3,y=0.0,z=0.55):
   pose_goal = geometry_msgs.msg.Pose()
   pose_goal.orientation.x = 1.0
@@ -26,7 +61,6 @@ def align_ft_z_at(x=0.3,y=0.0,z=0.55):
   move_group.go(wait=True)
   move_group.stop()
   move_group.clear_pose_targets()
-
 
 def reset_ft_gravity_aligned(mass=None):
 
@@ -47,11 +81,10 @@ def reset_ft_gravity_aligned(mass=None):
     gripper_mass = mass
 
   ft_offset.force.z = gripper_mass*9.81
-
   reset_ft_wrench_srv(ft_offset)
 
-
-def actuate_x_axis(distance=0.5, cycles=2):
+## Some actuation cycles I prepared earlier
+def actuate_x_axis(distance=0.05, cycles=2):
   waypoints = []
   wpose = move_group.get_current_pose().pose
  
@@ -70,7 +103,6 @@ def actuate_x_axis(distance=0.5, cycles=2):
     move_group.execute(plan, wait=True)
   else:
     print("ERROR: planning failed at %d of trajectory", fraction)
-
 
 def actuate_xz_axes(cycles=2):
   waypoints = []
@@ -96,7 +128,6 @@ def actuate_xz_axes(cycles=2):
   else:
     print("ERROR: planning failed at %d of trajectory", fraction)
 
-
 def actuate_joint_6(angle=pi/12, cycles=2):
   curJoints = move_group.get_current_state().joint_state.position
   move_group.go(joints=[curJoints[0], curJoints[1], curJoints[2], curJoints[3], curJoints[4], curJoints[5]+angle, curJoints[6]], wait=True)
@@ -114,7 +145,6 @@ def actuate_joint_6(angle=pi/12, cycles=2):
   curJoints = move_group.get_current_state().joint_state.position
   move_group.go(joints=[curJoints[0], curJoints[1], curJoints[2], curJoints[3], curJoints[4], curJoints[5]-angle, curJoints[6]], wait=True)
   move_group.stop()
-
 
 def actuate_phi_axis(cycles=2):
   waypoints = []
@@ -143,7 +173,6 @@ def actuate_phi_axis(cycles=2):
   else:
     print("ERROR: planning failed at %d of trajectory", fraction)
 
-
 def ft_orientation_cycle():
   curJoints = move_group.get_current_state().joint_state.position
   print("============ Press enter")
@@ -163,58 +192,39 @@ def ft_orientation_cycle():
   move_group.go(joints=[curJoints[0], curJoints[1], curJoints[2], curJoints[3], curJoints[4], curJoints[5], curJoints[6]+pi/2], wait=True)
   move_group.stop()
 
+#%%
 
-if __name__ == '__main__':
-  rospy.init_node('dlo_parameter_id', anonymous=True)
+rospy.init_node('fr3_control', anonymous=True)
 
-  print("============ Setting up moveit commander interface")
-  # MoveIt Commander interface init
-  moveit_commander.roscpp_initialize(sys.argv)
-  robot = moveit_commander.RobotCommander()
-  scene = moveit_commander.PlanningSceneInterface()
-  group_name = "fr3_arm"
-  move_group = moveit_commander.MoveGroupCommander(group_name)
-  move_group.set_max_acceleration_scaling_factor(0.3)
-  move_group.set_max_velocity_scaling_factor(0.3)
-  planning_frame = move_group.get_planning_frame()
-  print("============ Planning frame: %s" % planning_frame)
-  eef_link = move_group.get_end_effector_link()
-  print("============ End effector link: %s" % eef_link)
-  group_names = robot.get_group_names()
-  print("============ Available Planning Groups:", robot.get_group_names())
-  print("============ Robot state:")
-  print(robot.get_current_state())
-  print("")
-  # For displaying trajectory plans in RViz
-  display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                moveit_msgs.msg.DisplayTrajectory,
-                                                queue_size=20)
+print("============ Setting up moveit commander interface")
+# MoveIt Commander interface init
+moveit_commander.roscpp_initialize(sys.argv)
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group_name = "fr3_arm"
+move_group = moveit_commander.MoveGroupCommander(group_name)
+move_group.set_max_acceleration_scaling_factor(0.3) # Note these don't affect cartesian paths. Use retime_trajectory
+move_group.set_max_velocity_scaling_factor(0.3)     #
+planning_frame = move_group.get_planning_frame()
+print("============ Planning frame: %s" % planning_frame)
+eef_link = move_group.get_end_effector_link()
+print("============ End effector link: %s" % eef_link)
+group_names = robot.get_group_names()
+print("============ Available Planning Groups:", robot.get_group_names())
+print("============ Robot state:")
+print(robot.get_current_state())
+print("")
+# For displaying trajectory plans in RViz
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                              moveit_msgs.msg.DisplayTrajectory,
+                                              queue_size=20)
 
-  # Service for resetting FT sensor bias
-  reset_ft_wrench_srv = rospy.ServiceProxy('/bus0/ft_sensor0/reset_wrench', rokubimini_msgs.srv.ResetWrench())
+# Service for resetting FT sensor bias
+reset_ft_wrench_srv = rospy.ServiceProxy('/bus0/ft_sensor0/reset_wrench', rokubimini_msgs.srv.ResetWrench())
 
-  print("============ Press enter to vertically align and zero FT sensor")
-  input()
-  #move_group.go(joints=[0.0, -0.7156, 0.0, -1.693, 0.0, 0.9774, 0], wait=True) # EE close to [0.2, 0.0, 0.75] oriented Z down
-  move_group.go(joints=[0.0, -0.1, 0.0, -1.0, 0.0, 0.9, 0], wait=True) # EE close to [0.4, 0.0, 0.8] oriented Z down
-  move_group.stop()
-  curPos = move_group.get_current_pose().pose.position
-  align_ft_z_at(x=0.4,y=0.15,z=0.8)
-  # reset_ft_gravity_aligned(0.8281 + 0.078635) # object + FT adapter
-  
-  # print("============ Press enter to actuate x axis")
-  # input()
-  # actuate_x_axis(0.03,2)
-
-  # print("============ Press enter to actuate link 6")
-  # input()
-  # actuate_joint_6()
-
-  # print("============ Press enter to actuate xz axes")
-  # input()
-  # actuate_xz_axes(2)
-
-  #print("============ Press enter to actuate phi axis")
-  #input()
-  #actuate_phi_axis()
+#%%
+# Vertically align and zero FT sensor
+move_home(joint1=-pi/2)
+align_ft_z_at(x=0.0,y=-0.25,z=0.8)
+reset_ft_gravity_aligned(0.4 + 0.23 + 0.08) # cable length mass + end mass + FT adapter mass
   
